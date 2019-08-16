2e430d24195b41d03a8a54e1121dab96df71365a
Neal JIn Oh <no4259@gmail.com>
2019-08-16 16:30:31 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(sentence1)
   words = sentance.split("")
   new_words = []
   new_words = words.collect{|char| char.reverse}
+  new_words.join(" ")
 end
 
 # def reverse_each_word(sentence)
