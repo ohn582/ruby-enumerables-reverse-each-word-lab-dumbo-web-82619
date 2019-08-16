91c0db233c7bcce687562cbffc568e18bacdb94d
Neal JIn Oh <no4259@gmail.com>
2019-08-16 16:43:36 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,14 +2,6 @@ def reverse_each_word(sentence1)
   words = sentence1.split(" ")
   new_words = []
   new_words = words.collect{|char| char.reverse}
-  new_words.join(" ")
+  return new_words.join(" ")
 end
 
-
-# counter = 0
-# pets = ["Dog", "Cat", "Fish", "Bird", "Hamster"]
- 
-# while pets[counter] do
-#   puts pets[counter]
-#   counter += 1
-# end
